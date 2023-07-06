56400b32d65d9921e6121f3ab6d41365aab00eb6
tanvir-a0 <t.a.porva@gmail.com>
2023-07-07 00:41:09 +0600

    Uploading T4 and T5

---
+++ BasicJava/src/main/java/Product.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Acer
+ */
+
+//T5
+public class Product {
+    public static void main(String[] args){
+        System.out.println("id: 101");
+        System.out.println("title: iphone 14");
+        System.out.println("price: 1895 euros");
+        System.out.println("discription: perfect image with best image quality,");
+        System.out.println("catagory: iphone");
+    }
+    
+}
---
+++ BasicJava/src/main/java/basicjava/BioData.java
@@ -0,0 +1,15 @@
+
+package basicjava;
+
+/**
+ *
+ * @author Acer
+ */
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Anasul Islam");
+        System.out.print("01876218691");
+        
+    }
+    
+}
